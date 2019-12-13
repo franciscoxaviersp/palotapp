3508e2bfc59789c3efd695a442a411e464e632f4
PV <pedrovalerio@ua.pt>
2019-12-13 20:23:26 +0000

    tests

--- parkingalot/test_driver/app_test.dart
+++ parkingalot/test_driver/app_test.dart
@@ -27,8 +27,41 @@ void main() {
     });
 
     test('create account', () async {
-      await driver.tap(find.byValueKey('gotoreg'));
+      await driver.tap(find.byValueKey('userkey'));
       await driver.enterText('User');
+      await driver.tap(find.byValueKey('passkey'));
+      await driver.enterText('Pass');
+      await driver.tap(find.byValueKey('regbtn'));
+
+      await driver.waitFor(find.byValueKey('Alert'));
+      await driver.tap(find.byValueKey('close'));
+
+    });
+
+    test('login wrong pass', () async {
+      await driver.tap(find.byValueKey('userkey'));
+      await driver.enterText('User');
+      await driver.tap(find.byValueKey('passkey'));
+      await driver.enterText('');
+      await driver.tap(find.byValueKey('logbtn'));
+
+      await driver.waitFor(find.byValueKey('badpassAlert'));
+      await driver.tap(find.byValueKey('close'));
+    });
+
+    test('login right pass', () async {
+      await driver.tap(find.byValueKey('passkey'));
+      await driver.enterText('Pass');
+      await driver.tap(find.byValueKey('logbtn'));
+
+      await driver.waitFor(find.byType('HomePage'));
+    });
+
+    test('find parks', () async {
+      await driver.waitFor(find.byValueKey('onePark'));
+      await driver.tap(find.byValueKey('onePark'));
+
+      await driver.tap(find.byType('Parque'));
     });
 
   });
